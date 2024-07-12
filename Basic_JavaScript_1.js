ecedef582fcfd7a80582df8634fc48b3ccca8a62
EmmaElizabeth30 <144685453+EmmaElizabeth30@users.noreply.github.com>
2024-07-12 14:27:05 +0100

    Delete Basic_JavaScript_1.js

--- Basic_JavaScript_1.js
+++
@@ -1,7 +0,0 @@
-alert("Hello")
-
-function My_First_Function() {
-    var String= "Kiss me- I'm Irish!";
-    var result= String.fontcolor("green");
-    document.getElementById("irish").innerHTML= result;
-}
